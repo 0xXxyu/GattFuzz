83665faf59174922d89088d6ad78337dccdfc202
BaCde <Glacier@insight-labs.org>
2023-06-08 19:15:02 +0800

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fp:
 
 setup(
     name='gattfuzz',
-    version='0.0.5',
+    version='0.0.6',
     description= "A tool for fuzzing BLE GATT",
     long_description=description,
     packages=find_packages(),
