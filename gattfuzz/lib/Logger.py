6c8a895e93c7b8acbf3c8f08fc12fcc0b86b33f8
mang0cola <shjc404@gmail.com>
2023-08-30 15:02:56 +0800

    日志优化

--- gattfuzz/lib/Logger.py
+++ gattfuzz/lib/Logger.py
@@ -1,6 +1,52 @@
 import logging
-from colorama import Fore,Style
+import logging.handlers
+import copy
+import time
+import os
 
+# from colorama import Fore,Style
+def color(text, color_code):
+    """Colorize text.
+    @param text: text.
+    @param color_code: color.
+    @return: colorized text.
+    """
+    return "\x1b[%dm%s\x1b[0m" % (color_code, text)
+
+def red(text):
+    return color(text, 31)
+
+def green(text):
+    return color(text, 32)
+
+def yellow(text):
+    return color(text, 33)
+
+def blue(text):
+    return color(text, 34)
+
+
+class ConsoleHandler(logging.StreamHandler):
+    """Logging to console handler."""
+
+    def emit(self, record):
+        colored = copy.copy(record)
+
+        if record.levelname == "WARNING":
+            colored.msg = yellow(record.msg)
+        elif record.levelname == "ERROR" or record.levelname == "CRITICAL":
+            colored.msg = red(record.msg)
+        elif record.levelname == "INFO":
+            colored.msg = green(record.msg)
+        elif record.levelname == "DEBUG":
+            colored.msg = blue(record.msg)
+        else:
+            if "analysis procedure completed" in record.msg:
+                colored.msg = record.msg
+            else:
+                colored.msg = record.msg
+
+        logging.StreamHandler.emit(self, colored)
 
 class Logger():
     def __init__(self, loglevel=logging.INFO, loggername=None):
@@ -11,11 +57,19 @@ class Logger():
 
         if not self.logger.handlers:
             # 日志输出到控制台
-            stream_handler = logging.StreamHandler()
-            stream_handler.setLevel(loglevel)
-            stream_handler.setFormatter(formatter)
+            console_handler = ConsoleHandler()
+            console_handler.setFormatter(formatter)
+            # 日志保存到文件
+            log_dir_path = './logs'
+            if not os.path.exists(log_dir_path):
+                os.mkdir(log_dir_path)
+            log_file_path = os.path.join(log_dir_path, str(int(time.time())) + '.log')    
+            file_handler = logging.handlers.TimedRotatingFileHandler(log_file_path, when='D',interval=5)
+            file_handler.suffix = "%Y%m%d_%H%M.log"
+            file_handler.setFormatter(formatter)
 
-            self.logger.addHandler(stream_handler)
+            self.logger.addHandler(console_handler)
+            self.logger.addHandler(file_handler)
     
     def get_logger(self):
         return self.logger
